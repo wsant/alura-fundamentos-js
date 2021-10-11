dccd80d69460715404a0b97e64846968efe5367d
William Santos <william.s.s@outlook.com>
2021-10-11 03:59:26 -0300

    exemplos da aula 1

---
+++ type-string.js
@@ -0,0 +1,11 @@
+const texto1 = "Olá, mundo!";
+const texto2 = 'Olá, mundo!';
+const senha = "senhaSuperSegura4567";
+const StringDeNumeros = "34567";
+
+const citacao = "Meu nome é: ";
+const meuNome = "William";
+
+// concatenação (+)
+
+console.log(citacao + meuNome)
